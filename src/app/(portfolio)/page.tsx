147ff4f7873f2cd7fb6824e474933a969e7dd6b5
Nikita Vologdins <nvologdins@gmail.com>
2024-07-17 17:27:57 +0300

    skills are fetch from DB

--- src/app/(portfolio)/page.tsx
+++ src/app/(portfolio)/page.tsx
@@ -1,10 +1,13 @@
 import Intro from "../../components/intro/Intro";
 import Container from "../../components/Container";
+import { fetchDataOnServer } from "@/lib/utils";
+import { Skill as Skills } from "@/models/skills";
 
-export default function Home() {
+export default async function Home() {
+  const skills = await fetchDataOnServer(Skills);
   return (
     <Container className="h-dvh">
-      <Intro />
+      <Intro skills={skills} />
     </Container>
   );
 }
