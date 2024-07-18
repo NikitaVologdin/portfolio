d769dcfc7f23c479d6b8d0cf313da73f9c945d9c
Nikita Vologdins <nvologdins@gmail.com>
2024-07-18 11:54:30 +0300

    Created IfetchedDeveloper

--- src/types/Developer.ts
+++ src/types/Developer.ts
@@ -2,3 +2,9 @@ export interface IDeveloper extends Document {
   name: string;
   description: string;
 }
+
+export interface IFetchedDeveloper extends Document {
+  _id: string;
+  name: string;
+  description: string;
+}
