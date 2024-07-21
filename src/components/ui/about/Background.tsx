8263379540c7e05935aac0273529041a453eee92
Nikita Vologdins <nvologdins@gmail.com>
2024-07-21 13:03:26 +0300

    Background image and filters are set up

--- src/components/ui/about/Background.tsx
+++ src/components/ui/about/Background.tsx
@@ -10,7 +10,10 @@ export default function background({ path, children }: props) {
         "bg-contain bg-right bg-no-repeat h-full h-80 bg-cover bg-gradient-to-l flex flex-col justify-center items-center border-b border-[#e1e1e1]"
       }
       style={{
-        background: `linear-gradient(90deg, #FFF 0%, #FFF 55%, rgba(255, 255, 255, .6) 130%), no-repeat 110%  45% / 55% url('${path}')`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
+        backgroundImage: `linear-gradient(90deg, #FFF 0%, #FFF 55%, rgba(255, 255, 255, .6) 130%), url('${path}')`,
+        backgroundPosition: "right",
       }}
     >
       {children}
