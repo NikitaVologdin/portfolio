5aa238cef5a70c5509596f822ba5df528c816515
Nikita Vologdins <nvologdins@gmail.com>
2024-07-24 13:46:43 +0300

    favicons are updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,23 +17,23 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="scroll-smooth h-full">
       <head>
-        <link rel="icon" href="favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon/favicon.ico" sizes="any" />
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="apple-touch-icon.png"
+          href="/favicon/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="favicon-32x32.png"
+          href="/favicon/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="favicon-16x16.png"
+          href="/favicon/favicon-16x16.png"
         />
       </head>
       <body
