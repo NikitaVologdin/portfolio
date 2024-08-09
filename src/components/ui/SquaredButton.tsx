63d47ca950fa1c28543a2675461ee5ad63b5a740
Nikita Vologdins <nvologdins@gmail.com>
2024-08-09 10:22:25 +0300

    link preview issue resolved

--- src/components/ui/SquaredButton.tsx
+++ src/components/ui/SquaredButton.tsx
@@ -11,7 +11,7 @@ export default function SquaredButton({
 }: props) {
   return (
     <div
-      className={`relative p-[10px] border border-light-grey rounded-lg hover:border-[#8e8e8e] hover:after:absolute hover:after:inline-block hover:after:absolute hover:after:w-max hover:after:-bottom-7 hover:after:left-3.5 hover:after:bg-[#e1e1e1] hover:after:py-1 hover:after:px-2.5 hover:after:border hover:after:border-[#e1e1e1] hover:after:rounded-2xl hover:after:text-sm hover:after:z-10 hover:after:content-[attr(data-help)]
+      className={`relative p-[10px] border border-light-grey rounded-lg hover:border-[#8e8e8e] hover:after:absolute hover:after:inline-block hover:after:absolute hover:after:w-max hover:after:-bottom-7 hover:after:-right-20 hover:after:bg-[#e1e1e1] hover:after:py-1 hover:after:px-2.5 hover:after:border hover:after:border-[#e1e1e1] hover:after:rounded-2xl hover:after:text-sm hover:after:z-10 hover:after:content-[attr(data-help)]
       ${className}`}
       data-help={backdropText}
     >
