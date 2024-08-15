8933aa856c60b9e972496f595e5cce1a3a6dbb57
Nikita Vologdins <nvologdins@gmail.com>
2024-08-15 17:27:11 +0300

    change function for fetching developers info

--- src/app/(portfolio)/portfolio/page.tsx
+++ src/app/(portfolio)/portfolio/page.tsx
@@ -28,7 +28,7 @@ export default async function page() {
   );
 
   const skills = await cachedSkills();
-  const [developer] = await cachedSkills();
+  const [developer] = await cachedDeveloper();
 
   return (
     <Container className="h-dvh my-auto">
