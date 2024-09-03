ec457a31eba7f03dccb5bb560fd09e7f84bac3f4
Nikita Vologdins <nvologdins@gmail.com>
2024-09-03 16:50:05 +0300

    viewer

--- src/components/ui/screenshots/ScreenshotViewer.tsx
+++ src/components/ui/screenshots/ScreenshotViewer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ClosingFormIcon from "@/components/ui/form/ClosingFormIcon";
-import ArrowToLeftSVG from "./ArrowToLeftSVG";
-import ArrowToRightSVG from "./ArrowToRightSVG";
+import ArrowToLeftSVG from "./ToLeftSVG";
+import ArrowToRightSVG from "./ToRightSVG";
 import Image from "next/image";
 
 interface props {
